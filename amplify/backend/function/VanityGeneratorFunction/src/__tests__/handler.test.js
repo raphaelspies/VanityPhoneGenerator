0247b6aca153a5980de34c3e2e7f6c6dc18b91ab
raphspies <raphspies@gmail.com>
2021-10-30 19:43:23 -0700

    tests no check whether invalid numbers throw the correct error

--- amplify/backend/function/VanityGeneratorFunction/src/__tests__/handler.test.js
+++ amplify/backend/function/VanityGeneratorFunction/src/__tests__/handler.test.js
@@ -1,6 +1,7 @@
 const {handler} = require('../index');
 const eventNoVanity = require('./event-noVanity');
 const eventHasVanity = require('./event-hasVanity');
+const eventInvalidNo = require('./event-invalidNo')
 
 
 test('Valid phone number with no vanity numbers', async () => {
@@ -8,7 +9,6 @@ test('Valid phone number with no vanity numbers', async () => {
     "CustomerNumber": "+16073424400",
     "VanityNumbers": "No valid vanity numbers"
   };
-  console.log(JSON.stringify(handler))
   const result = await handler(eventNoVanity);
   expect(result).toStrictEqual(expectedResult);
 });
@@ -19,7 +19,32 @@ test('Valid phone number with vanity numbers', async () => {
     "CustomerNumber": "+18003569377",
     "VanityNumbers": "1-800-flowers, 1-800-3-lowers"
   };
-  console.log(JSON.stringify(handler))
   const result = await handler(eventHasVanity);
   expect(result).toStrictEqual(expectedResult);
+})
+
+test('Invalid phone number fails', async () => {
+  expect.assertions(1) // make sure one assertion is called
+  try {
+    await handler(eventInvalidNo);
+  } catch (e) {
+    expect(e).toEqual(new Error('Phone number is invalid')
+    )
+  }
+
+  // expect(async () => {
+  //   await handler(eventInvalidNo);
+  // }).toThrow(new Error())
+
+
+  // expect(async () => {
+  //   const result =  await handler(eventInvalidNo);
+  //   return result;
+  // }).rejects.toThrow(new Error())
+
+
+  // toThrow(Error("Phone number is invalid"))
+  // .catch(err => {
+  //   expect(err).toEqual(new Error())
+  // })
 })
